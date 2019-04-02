3bdba26409a7194c45f7c0185c920d647cbe7bb6
muratsigmaz <48977862+muratsigmaz@users.noreply.github.com>
2019-04-02 18:59:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -196,13 +196,63 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
-		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
-		}
-	}
+  if (msg.content === 'sa') {
+    msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Sa') {
+    msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Selam') {
+    msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Selamlar') {
+    msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'SA') {
+    msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'sunucu') {
+    msg.reply('sen sunucuyu kafana takma biz büyüyoruz bak dalgana genç!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'murat') {
+    msg.reply('yavaş ol murat derken bile besmele çekmiyorsun sakin bölgeye geç!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'link') {
+    msg.reply('hayır anlamıyorum napcan linki arkadaşını çağırcaksan tamam ama baskın filan sker atarım!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'naber') {
+    msg.reply('gelmedi senden bi haber merak ettim öldünmü diye sorcam banane mk!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!ban özel') {
+    msg.reply('AL BUNU AĞZINA https://media.giphy.com/media/uC9e2ojJn1ZXW/giphy.gif');
+  }
 });
 
 
